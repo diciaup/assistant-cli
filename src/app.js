a01c9f098b7fe4a5ebdf9484b916a65604b803cb
Paolo <diciaula.paolo@gmail.com>
2022-12-12 13:27:24 +0100

    hotfix: change cookies management, introducing cf_clearance

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const loadingSpinner = new Spinner('processing... %s');
 loadingSpinner.setSpinnerString('|/-\\');
 const localStorageLocation = `${__dirname}/../localStorage`;
 
-function execElectron(path, options, callback) {
+function execElectron(path, options, callback, clearCache = false) {
 
   if (!path) {
       return callback(new Error("The path argument is mandatory."));
@@ -29,20 +29,21 @@ function execElectron(path, options, callback) {
   var cwd = options.cwd || process && process.cwd() || __dirname;
   delete options.cwd;
 
-  return spawn(electronPath, oArgv(options), { cwd: cwd, _showOutput: process.env.ENV === 'dev'}, callback);
-};
+  return spawn(electronPath, oArgv(options), { cwd: cwd, env: {CLEAR_CACHE: clearCache}, _showOutput: process.env.ENV === 'dev'}, callback);
+}
 
-const getToken = () => {
-  const childProcess = execElectron(`${__dirname}/fetch-token.js`);
+const getToken = (clearCache) => {
+  const childProcess = execElectron(`${__dirname}/fetch-token.js`, undefined, undefined, clearCache);
   return new Promise((resolve, reject) => {
     childProcess.stdout.on('data', (message) => {
       try {
-        const token = JSON.parse(message).token;
+        const token = JSON.parse(message.toString());
         if(token) {
-          fs.writeFileSync(localStorageLocation, token);
+          fs.writeFileSync(localStorageLocation, message);
           resolve(token);
         }
       } catch(e) {
+        console.log('original message', message.toString());
         reject(e);
       }
     })
@@ -52,20 +53,21 @@ const getToken = () => {
 
 const getClient = async () => {
   const {ChatGPTAPI} = await import('chatgpt');
-  let token;
+  let tokens;
   try {
-    token = fs.readFileSync(localStorageLocation).toString();
+    tokens = JSON.parse(fs.readFileSync(localStorageLocation).toString());
   } catch(e) {}
-
-  if(!token) {
-    token = await getToken();
+  if(!tokens) {
+    tokens = await getToken(false);
   }
   const api = new ChatGPTAPI({
-    sessionToken: token
+    sessionToken: tokens.token,
+    clearanceToken: tokens.clearanceToken,
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
   });
   const authenticated = await api.getIsAuthenticated();
   if (!authenticated) {
-    token = await getToken();
+    tokens = await getToken(false);
     return getClient();
   }
   return api;
@@ -79,7 +81,7 @@ const useConversation = (conversationApi, rl, answer = "Hello how can i help you
         useConversation(conversationApi, rl, res);
       })
       .catch(e => useConversation(conversationApi, rl, e))
-      .finally(() => loadingSpinner.stop(true));   
+      .finally(() => loadingSpinner.stop(true));
     }else {
       useConversation(conversationApi, rl, 'Please write a message');
     }
@@ -95,11 +97,17 @@ const startConversation = (conversationApi) => {
   useConversation(conversationApi, rl);
 }
 
+const resetAuth = async () => {
+  fs.rmSync(localStorageLocation);
+  await getToken(true);
+}
+
 
 const commands = {
   'open chat': startConversation,
   'start conversation': startConversation,
   'start chat': startConversation,
+  'reset auth': resetAuth,
   'chat': startConversation
 };
 
--- src/fetch-token.js
+++ src/fetch-token.js
@@ -2,13 +2,21 @@ const { app, BrowserWindow } = require('electron');
 
 const getSessionToken = async () => {
     const win = new BrowserWindow({width: 799, height: 600});
-    win.loadURL('https://chat.openai.com/chat');
+    win.loadURL('https://chat.openai.com/chat', {userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'});
+    if(process.env.CLEAR_CACHE === 'true') {
+        await win.webContents.session.clearCache();
+        await win.webContents.session.clearStorageData({origin: 'https://chat.openai.com'});
+        await win.webContents.session.clearAuthCache();
+        win.close();
+        app.exit();
+    }
     win.webContents.on('did-finish-load', async () => {
         const cookies = await win.webContents.session.cookies.get({});
         try {
             const token = cookies.filter((cookie) => cookie.name === '__Secure-next-auth.session-token')[0].value;
+            const clearanceToken = cookies.filter((cookie) => cookie.name === 'cf_clearance')[0].value;
             if(token) {
-                process.stdout.write(JSON.stringify({token}));
+                process.stdout.write(JSON.stringify({token, clearanceToken}));
                 if(process.env.ENV !== 'dev') {
                     win.close();
                     app.exit();
