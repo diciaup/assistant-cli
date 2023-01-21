a53022d58d88ef2998e2675f83f47cb808dd3a67
diciaup <diciaula.paolo@gmail.com>
2023-01-22 00:26:48 +0100

    feat: implement continuos conversation feature

--- src/browser-commands/send-message.ts
+++ src/browser-commands/send-message.ts
@@ -6,7 +6,8 @@ export const sendMessage = async (message: string[]): Promise<string> => {
     win.loadURL(CHAT_GPT_DOMAIN);
     return new Promise((resolve) => {
         win.webContents.on('did-finish-load', async () => {
-            let code = `(!!document.querySelector('meta[content="ChatGPT"]') && !!document.getElementsByTagName("textarea").item(0));`;
+            console.log('finish load');
+            let code = `(!!document.querySelector('meta[content="ChatGPT"]') && !!document.getElementsByTagName("textarea").item(0) && !!document.getElementsByTagName("nav").item(0));`;
             const executionResult = await win.webContents.executeJavaScript(code);
             if(executionResult) {
                 if(process.env.ENV !== 'dev') {
@@ -18,10 +19,51 @@ export const sendMessage = async (message: string[]): Promise<string> => {
     })
 };
 
-const handleMainPage = (win: BrowserWindow, message: string): Promise<string> => {
+const selectNextConversation = async (win: BrowserWindow, index = 0) => {
+    console.log('selecting ', index);
+    return new Promise(async (resolve) => {
+        win.webContents.debugger.on('message', async (event, method, params) => {
+            console.log(event, method, params);
+            if (method === 'Network.responseReceived') { 
+                const {response} = params;
+                const {url} = response;
+                if (url.indexOf("/conversation") > -1 && !url.endsWith('conversation')) {
+                    resolve(null);
+                }
+            }
+        });
+        const code = `document.getElementsByTagName('nav').item(0).getElementsByClassName("flex-col").item(0).childNodes.item(0).getElementsByTagName("a").item(${index}).click()`;
+        await win.webContents.executeJavaScript(code);
+    });
+}
+
+const findConversation = async (win: BrowserWindow) => {
+    let index = 0;
+    let executionResult = true;
+    const navItems = await new Promise(resolve => setInterval(async () => {
+        const code = `document.getElementsByTagName('nav').item(0).getElementsByClassName("flex-col").item(1).childElementCount`;
+        const length = await win.webContents.executeJavaScript(code);
+        if (!!length) {
+            resolve(length);
+        }
+    }, 1000));
+    if(navItems > 1) {
+        while(executionResult) {
+            await selectNextConversation(win, index);
+            let code = `(!!document.querySelector('meta[content="ChatGPT"]') && !!document.getElementsByTagName("textarea").item(0) && !!document.getElementsByTagName("nav").item(0));`;
+            executionResult = !(await win.webContents.executeJavaScript(code));
+            console.log('check index', index, executionResult);
+            index++;
+        }
+    }
+}
+
+
+
+const handleMainPage = async (win: BrowserWindow, message: string): Promise<string> => {
     win.webContents.debugger.attach('1.3');
     let requestId;
-    return new Promise((resolve) => {
+    return new Promise(async (resolve) => {
         win.webContents.debugger.on('message', async (event, method, params) => {
             if (method === 'Network.responseReceived') { 
                 const { response } = params;
@@ -35,8 +77,7 @@ const handleMainPage = (win: BrowserWindow, message: string): Promise<string> =>
                     const currentMessageContent = await win.webContents.executeJavaScript(`
                         document.getElementsByClassName('markdown').item(document.getElementsByClassName('markdown').length - 1).innerHTML;
                     `);
-                    process.stdout.write(JSON.stringify({return: currentMessageContent}))
-
+                    process.stdout.write(JSON.stringify({return: currentMessageContent}));
                 }catch(e) {
                     console.error(e);
                 }
@@ -48,6 +89,7 @@ const handleMainPage = (win: BrowserWindow, message: string): Promise<string> =>
             }
         })
         win.webContents.debugger.sendCommand('Network.enable');
+        await findConversation(win);
         win.webContents.executeJavaScript(`
             document.getElementsByTagName("textarea").item(0).value = "${message}";
             document.getElementsByTagName("textarea").item(0).nextElementSibling.click();
